6047b4f422bb0ef0045e62240a73f306b5ec7066
GGolfz <wisarutgolf11@gmail.com>
2021-11-27 20:53:39 +0700

    fix :hammer:

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
-require('dotenv').config();
-const {Client, Intents} = require('discord.js');
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-const wordList = require('./wordList.json');
+const wordList = require("./wordList.json");
 
 const getWordListByTopic = (topic) => {
-    if(topic === "random") return wordList.map(word => word.word);
-    return wordList.filter(word => word.topic.toLowerCase() === topic.toLowerCase()).map(word => word.word);
-}
+  if (topic === "random") return wordList.map((word) => word.word);
+  return wordList
+    .filter((word) => word.topic.toLowerCase() === topic.toLowerCase())
+    .map((word) => word.word);
+};
 
 const getWordByTopic = (topic) => {
-    let wordList = getWordListByTopic(topic);
-    return wordList[Math.floor(Math.random() * wordList.length)];
-}
+  let wordList = getWordListByTopic(topic);
+  return wordList[Math.floor(Math.random() * wordList.length)];
+};
 
-client.on('ready', () => {
-    console.log('Opbot is ready !');
+client.on("ready", () => {
+  console.log("Opbot is ready !");
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
+  if (message.content.startsWith("!")) {
     let messageList = message.content.split(" ");
     let topic = "random";
     if (messageList[0] === "!fword") {
-        if(!messageList[1].startsWith("<")) {
-            topic = messageList[1];
-            console.log(topic);
-        }
-    }
-    let gameData = {};
-    let usedWord = [];
-    message.mentions.users.forEach(user => {
+      if (!messageList[1].startsWith("<")) {
+        topic = messageList[1];
+        console.log(topic);
+      }
+      let gameData = {};
+      let usedWord = [];
+      message.mentions.users.forEach((user) => {
         let randomWord = getWordByTopic(topic);
-        while(usedWord.includes(randomWord)) {
-            randomWord = getWordByTopic(topic);
+        while (usedWord.includes(randomWord)) {
+          randomWord = getWordByTopic(topic);
         }
-        gameData[user.id] = {name: user.username, word: randomWord};
+        gameData[user.id] = { name: user.username, word: randomWord };
         usedWord.push(randomWord);
-    })
-    message.mentions.users.map(user => {
+      });
+      message.mentions.users.map((user) => {
         let message = "";
-        for(let i in gameData) {
-            if(i != user.id) {
-                message += gameData[i].name + ": " + gameData[i].word + "\n";
-            }
+        for (let i in gameData) {
+          if (i != user.id) {
+            message += gameData[i].name + ": " + gameData[i].word + "\n";
+          }
         }
         user.send(message);
-    })
-    let time = 300;
-    let sendMessage = await message.channel.send("Game started ! Time left: 5:00");
-    let timer = setInterval(() => {
+      });
+      let time = 300;
+      let sendMessage = await message.channel.send(
+        "Game started ! Time left: 5:00"
+      );
+      let timer = setInterval(() => {
         time -= 1;
         let timeString = "";
         if (time > 60) {
-            timeString = Math.floor(time / 60) + ":" + (time % 60);
+          timeString = Math.floor(time / 60) + ":" + (time % 60);
         } else {
-            timeString = time + "s";
+          timeString = time + "s";
         }
         sendMessage.edit("Game started ! Time left: " + timeString);
-        if(time == 0) {
-            sendMessage.edit("Game ended !");
-            clearInterval(timer);
+        if (time == 0) {
+          sendMessage.edit("Game ended !");
+          clearInterval(timer);
         }
-    },1000)
-})
-
+      }, 1000);
+    }
+  }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
